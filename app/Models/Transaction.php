18b55e031f2a4be175fd7b2cc31b00d0300bf1fc
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-11-13 19:08:35 -0300

    feat: add methods and fillables in transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'payer_account_id',
+        'receiver_account_id',
+        'value'
+    ];
+
+    public function accountPayer() : belongsTo
+    {
+        return $this->belongsTo(Account::class, 'payer_account_id');
+    }
+
+    public function accountReceiver() : belongsTo
+    {
+        return $this->belongsTo(Account::class,'receiver_account_id');
+    }
 }
