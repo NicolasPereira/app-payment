321c1547b47b15cc2123dff3127f217b24089295
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-11-13 01:12:22 -0300

    fix: corrigindo migration

--- database/migrations/2021_11_13_033950_add_profile_to_users_table.php
+++ database/migrations/2021_11_13_033950_add_profile_to_users_table.php
@@ -14,7 +14,7 @@ class AddProfileToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profile')->nullable();       
+            $table->string('profile')->after('email')->default('client');       
         });
     }
 
@@ -26,7 +26,7 @@ class AddProfileToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('profile');
         });
     }
 }
