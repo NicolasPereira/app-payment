8e7a8ea4f920dee7de0ada429406675126847726
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-11-14 16:49:46 -0300

    fix: corrigindo a ordemd e validação

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InsufficientCashException;
+use App\Exceptions\PayeeAndPayerIsSameException;
+use App\Exceptions\PayeeExistsException;
+use App\Exceptions\PayerExistsException;
+use App\Exceptions\ShopkepperMakeTransactionException;
+use App\Http\Requests\TransactionPostRequest;
+use App\Repositories\TransactionRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-
 class TransactionController extends Controller
 {
-    //
+    /**
+     * @var TransactionRepository
+     */
+    private $transactionRepository;
+
+    public function __construct(TransactionRepository $transactionRepository)
+    {
+        $this->transactionRepository = $transactionRepository;
+    }
+
+    public function create(TransactionPostRequest $request): JsonResponse
+    {
+        $payload = [
+                'payer_id' => $request->payer,
+                'payee_id' => $request->payee,
+                'value' => $request->value
+        ];
+        try{
+            $transaction = $this->transactionRepository->index($payload);
+            return response()->json($transaction);
+        }catch(\Exception $exception){
+            return response()->json(['errors' => ['message' => $exception->getMessage()]], $exception->getCode());
+        }
+
+
+    }
 }
