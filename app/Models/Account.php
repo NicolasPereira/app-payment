e29f3f8cd12fb316edf95b2a3f4387ca3a9758e4
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-11-14 22:23:58 -0300

    feat: add trait para gerar Uuid

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\hasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Account extends Model
 {
     use HasFactory;
+    use hasUuid;
 
     public $incrementing = false;
 
