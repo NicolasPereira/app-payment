f246e4ffb6db8ffede2db0fe159e352e2ac8791e
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-11-16 18:19:52 -0300

    test: implementando funções auxiliares

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use App\Models\Account;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function createUser(array $options = [])
+    {
+        return User::factory()->create($options);
+    }
+
+    protected function addCashAccount(Account $account, $value):void
+    {
+        $account->balance = $value;
+        $account->save();
+    }
 }
