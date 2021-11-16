981cb2dce6b957777a469813fc8ba5a3927aa3b8
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-11-16 19:10:03 -0300

    feat: adicionando nome para a rota

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/transaction',[TransactionController::class,'create']);
+Route::post('/transaction',[TransactionController::class,'create'])->name('transaction');
