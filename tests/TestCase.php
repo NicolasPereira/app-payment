9f506b9ff260001c9f727132b0710a4c9534d117
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2021-11-17 01:48:49 -0300

    test: adicionando criação do transaction

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\Models\Account;
+use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -15,9 +16,19 @@ abstract class TestCase extends BaseTestCase
         return User::factory()->create($options);
     }
 
+    protected  function createTransaction(array $options = [])
+    {
+        return Transaction::factory()->create($options);
+    }
+
     protected function addCashAccount(Account $account, $value):void
     {
         $account->balance = $value;
         $account->save();
+    }
+
+    protected function saveEntity($model):void
+    {
+        $model->save();
     }
 }
